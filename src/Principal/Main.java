0f1406487b122b2806ebc88accb14a8d9cea6f1a
EDUARDO RODIGUEZ <erodriguezp2001@alumno.ipn.mx>
2024-12-30 14:47:53 -0600

    Menu

---
+++ src/Principal/Main.java
@@ -0,0 +1,17 @@
+package Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("************************************************************");
+        System.out.println("Sea bienveinido/a al Conversor de Monedas =) \n");
+        System.out.println(" 1) Dólar =>> Peso Argentino");
+        System.out.println(" 2) Peso Argentino =>> Dólar");
+        System.out.println(" 3) Dólar =>> Real Brasileño");
+        System.out.println(" 4) Real Brasileño =>> Dólar");
+        System.out.println(" 5) Dólar =>> Peso Colombiano");
+        System.out.println(" 6) Peso Colombiano =>> Dólar");
+        System.out.println(" 7) Salir");
+        System.out.println(" Elija una opcion válida:");
+        System.out.println("************************************************************");
+    }
+}
